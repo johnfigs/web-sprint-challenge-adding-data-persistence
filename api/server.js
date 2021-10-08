e814ae496158cf8f76076b4b26f7ef02057d0490
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-10-08 10:47:57 -0700

    completed server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require('express')
+const helmet = require('helmet')
+
+const resourceRouter = require('./resource/router')
+const projectRouter = require('./project/router')
+const taskRouter = require('./task/router')
+
+const server = express()
+
+server.use('/api/resources', resourceRouter)
+server.use('/api/projects', projectRouter)
+server.use('/api/tasks', taskRouter)
+
+server.use(helmet())
+server.use(express.json())
+
+module.exports = server
