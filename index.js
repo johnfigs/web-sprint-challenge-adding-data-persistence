fc7b2c34cbbf47697c4bb73938de2b58c57510dc
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-10-08 10:41:29 -0700

    setup package.json and completed index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // start your server here
+require('dotenv').config()
+
+const server = require('./api/server')
+
+const port = process.env.port
+
+server.listen(port, () => {
+    console.log(`***listening on port ${port}`)
+})
